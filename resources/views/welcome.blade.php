b7f87873eed7db31ad39a778c7a3180a7fb632c6
anas zakia <anaszakia123@gmail.com>
2025-12-03 18:55:32 +0700

    fix deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,7 +73,7 @@
                     <div class="inline-block">
                         <h1 class="text-7xl sm:text-8xl lg:text-9xl font-black tracking-tighter">
                             <span class="block text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 via-blue-500 to-cyan-600 drop-shadow-2xl">
-                                RIVANA
+                                RIVANA auto deploy
                             </span>
                         </h1>
                         <!-- Animated underline -->
