90acd12e6e840ef3fd92c5024b67755ee628721d
anaszakia123 <anaszakia123@gmail.com>
2026-06-26 13:26:23 +0700

    fix: coba ga

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RIVANA - River DNA Analysis</title>
+    <title>RIVANA - River DNA Analysis (TES ACTION)</title>
     <link rel="icon" type="image/jpeg" href="/images/logo2.jpg">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
